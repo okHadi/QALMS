4632a39eeabb1d519e2d99c1f7ae9b918eaacc6d
okHadi <khan.hadi2951@gmail.com>
2022-12-17 03:40:41 +0500

    Succesful logging, routing bugged

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
-from flask import Flask, render_template
+from logindata import logging
+from flask import Flask, render_template, request, redirect, url_for    #render_template() looks for a template (HTML file) in the templates folder.
 app = Flask(__name__)   #creates a flask object that will be run
 
 
 
-@app.route('/login')         #main route
-def root():
-    return render_template("login.html")          #we can pass the values to be shown in the login.html as parameters of render_template
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if logging(request.form['username'], request.form['password']) == "Login failed":
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template("login.html", error=error)          #we can pass the values to be shown in the login.html as parameters of render_template
 
 
-
+app.route('/')
+def home():
+    return render_template("home.html")
 
 
 
