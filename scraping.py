9f8464db54fc924632fe935869d042b686df5fca
muzamilkm <mkaleem.bscs22seecs@seecs.edu.pk>
2022-12-25 14:09:14 +0500

    Getting results from the tables on links and printing them

--- scraping.py
+++ scraping.py
@@ -90,8 +90,8 @@ def login_and_scrape(username, password):
     session.close()
     # Close the browser
     driver.close()
-username='mkaleem.bscs22seecs'
-password='4IE8bhkp1234!@#$'
+username='USERNAME'
+password='PASSWORD'
 login_and_scrape(username, password)
 
 
