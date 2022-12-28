1def50996d9275a7be86f33cf187352c75f4d1a7
okHadi <khan.hadi2951@gmail.com>
2022-12-29 01:21:31 +0500

    Simple html page

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from logindata import logging
 from scraping import login_and_scrape
 from flask import Flask, render_template, request, redirect, url_for, session    #render_template() looks for a template (HTML file) in the templates folder.
 app = Flask(__name__)   #creates a flask object that will be run
+
 app.secret_key = 'eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTY3MjE2MTYwMSwiaWF0IjoxNjcyMTYxNjAxfQ.uqmoow_iw48tGBIsBlcc_rWcqEVST_-k9r1yolp21Yw' #creates a secret key for the session
 
 
@@ -11,10 +12,15 @@ def login():
     error = None
     if request.method == 'POST':
         username = request.form['username']
-        password = request.form['password']
+        qalampass = request.form['qalampass']
+        lmspass = request.form['lmspass']
         session['username'] = username
-        session['password'] = password
-        if logging(username, password) == "Login failed":
+        session['qalampass'] = qalampass
+        session['lmspass'] = lmspass
+            # Validate the form data
+        if not all([username, qalampass, lmspass]):
+            error = 'All fields are required'
+        elif logging(username, qalampass) == "Login failed":
             error = 'Invalid Credentials. Please try again.'
         else:
             session['logged_in'] = True
@@ -28,10 +34,9 @@ def home():
         return redirect(url_for('login'))
     else:
         username = session.get('username', '')
-        password = session.get('password', '')
-        login_and_scrape(username, password)
+        qalampass = session.get('qalampass', '')
+        login_and_scrape(username, qalampass)
         return render_template("home.html")
-
 
 
 
