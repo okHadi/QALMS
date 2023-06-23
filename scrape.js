a0975911bd6a9224b7fa9f274be25379beefd5c5
muzamilkm <mkaleem.bscs22seecs@seecs.edu.pk>
2023-06-23 18:03:22 +0500

    Initial scrape setup

--- scrape.js
+++ scrape.js
@@ -18,14 +18,14 @@ const password = 'ILIPtphi180M!';
   await page.click('.btn.btn-nust.btn-block.py-3.mt-4');
 
   // Modify the code to wait for a specific element to appear after login
-  await page.waitForSelector('#user_heading_content');
+  // await page.waitForSelector('#user_heading_content', {timeout: 60000});
+  await page.waitForNavigation();
 
   const html = await page.content();
 
   const $ = cheerio.load(html);
 
   // Perform scraping operations using Cheerio
-  // Example: Extract the text content of a specific element
   const elementText = $('#user_heading_content').text();
 
   console.log(elementText);
